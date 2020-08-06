8f9c08da5ec6737ec7cdbda7f31d7ccfc91869ad
xxx <xxx>
2020-08-06 10:04:05 +0200

    init
    init

---
+++ Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace ArcToCore.Core.HandballBoard.VideoCutter
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Defines the checkVideoList.
+        /// </summary>
+        private static List<string> checkVideoList = new List<string>();
+
+        /// <summary>
+        /// The DisplayMenu.
+        /// </summary>
+        static public void DisplayMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("ArcToCore Video Cutter Version 1.3");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// The AddTribleQuotes.
+        /// </summary>
+        /// <param name="value">The value<see cref="string"/>.</param>
+        /// <returns>The <see cref="string"/>.</returns>
+        public static string AddTribleQuotes(this string value)
+        {
+            return "\"\"" + value + "\"\"";
+        }
+
+        /// <summary>
+        /// The AddDoubleQuotes.
+        /// </summary>
+        /// <param name="value">The value<see cref="string"/>.</param>
+        /// <returns>The <see cref="string"/>.</returns>
+        public static string AddDoubleQuotes(this string value)
+        {
+            return "\"" + value + "\"";
+        }
+
+        /// <summary>
+        /// The Main.
+        /// </summary>
+        private static void Main()
+        {
+            GenerateVideo();
+            int count = 10;
+            bool run = true;
+            while (run)
+            {
+                if (count == 0)
+                {
+                    run = false;
+                }
+
+                if (!IsVideoGeneratingComplete())
+                {
+                    GenerateVideo();
+                }
+                else
+                {
+                    run = false;
+                }
+
+
+                count--;
+            }
+        }
+
+        /// <summary>
+        /// The IsVideoGeneratingComplete.
+        /// </summary>
+        /// <returns>The <see cref="bool"/>.</returns>
+        private static bool IsVideoGeneratingComplete()
+        {
+            var alreadyGeneratedFiles = Directory.GetFiles(Environment.CurrentDirectory + @"\VideoOutput\", "*.mp4*").ToList();
+
+            if (checkVideoList.Count != alreadyGeneratedFiles.Count)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// The GenerateVideo.
+        /// </summary>
+        private static void GenerateVideo()
+        {
+            List<string> pausedFilesList = new List<string>();
+            int videoFormat = 1;
+
+            string[] mp4File = Directory.GetFiles(Environment.CurrentDirectory + @"\VideoFile\", "*.mp4");
+
+            if (string.IsNullOrEmpty(mp4File[0]))
+            {
+                Console.WriteLine("VideoFile folder contains no mp4 files.");
+                return;
+            }
+
+            string[] filePaths = Directory.GetFiles(Environment.CurrentDirectory + @"\VideoTrackInput\", "*.json");
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            var alreadyGeneratedFiles = Directory.GetFiles(Environment.CurrentDirectory + @"\VideoOutput\", "*.*").ToList();
+
+            string line;
+            var path = Environment.CurrentDirectory + @"\";
+
+            foreach (var fileItem in filePaths)
+            {
+                StreamReader file = new StreamReader(fileItem);
+                line = file.ReadToEnd();
+
+                var pathFmpeg = path + "ffmpeg.exe";
+
+                var tracks = line.Split("|");
+
+                foreach (var trackSegment in tracks)
+                {
+                    if (string.IsNullOrEmpty(trackSegment))
+                    {
+                        continue;
+                    }
+
+                    if (trackSegment.Equals(" "))
+                    {
+                        continue;
+                    }
+
+                    string filename = trackSegment.Replace(@"\r", "").Replace(@"\n", "").Trim().Split(' ').GetValue(0).ToString().Trim();
+
+                    FileInfo fi = new FileInfo(filename);
+
+                    checkVideoList.Add(fi.Name);
+
+                    if (alreadyGeneratedFiles.Any(x => new FileInfo(x).Name.Split('.').GetValue(0).ToString().Equals(fi.Name.Split('.').GetValue(0).ToString())))
+                    {
+                        //Console.WriteLine(fi.Name + " already exsists. Try delete the file.");
+                        continue;
+                    }
+
+                    string split1 = trackSegment.Split(' ').GetValue(1).ToString();
+
+                    string split2 = trackSegment.Split(' ').GetValue(3).ToString();
+
+                    if (split1.Equals(split2))
+                    {
+                        Console.WriteLine("Split time for:" + split1 + " is equal to:" + split2);
+                        continue;
+                    }
+
+                    string oldFile = string.Empty;
+
+                    if (videoFormat.Equals(1))
+                    {
+                        oldFile = path + "VideoOutput\\" + filename + ".mp4";
+                    }
+
+                    if (File.Exists(oldFile))
+                    {
+                        File.Delete(oldFile);
+                    }
+                    Thread.Sleep(10000);
+
+                    if (videoFormat.Equals(1))
+                    {
+                        startInfo.Arguments = " -i " + AddDoubleQuotes(mp4File[0]) + " -ss " + split1 + " -to " + split2 + " -c:v libx265 -crf 28 -c:a aac -b:a 128k " + AddDoubleQuotes(path + "VideoOutput\\" + filename + ".mp4");
+                    }
+
+                    startInfo.CreateNoWindow = false;
+                    startInfo.UseShellExecute = false;
+                    startInfo.FileName = pathFmpeg;
+                    startInfo.RedirectStandardOutput = true;
+
+                    Console.WriteLine(string.Format(
+                        "Executing \"{0}\" with arguments \"{1}\".\r\n",
+                        startInfo.FileName,
+                        startInfo.Arguments));
+
+                    try
+                    {
+                        Console.Write("Prepare to generate video tracks....");
+                        using (var progress = new ProgressBar())
+                        {
+                            for (int i = 0; i <= 100; i++)
+                            {
+                                progress.Report((double)i / 100);
+                                Thread.Sleep(100);
+                            }
+                        }
+                        Console.WriteLine("Ready.");
+
+                        using (Process process = Process.Start(startInfo))
+                        {
+                            while (!process.StandardOutput.EndOfStream)
+                            {
+                                string linex = process.StandardOutput.ReadLine();
+                                Console.WriteLine(linex);
+                            }
+
+                            process.WaitForExit();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.Message.Equals("Access is denied"))
+                        {
+                            pausedFilesList.Add(startInfo.Arguments);
+                        }
+                    }
+                }
+
+                if (pausedFilesList.Count > 0)
+                {
+                    try
+                    {
+                        foreach (var item in pausedFilesList)
+                        {
+                            startInfo.Arguments = item;
+                            using (Process process = Process.Start(startInfo))
+                            {
+                                while (!process.StandardOutput.EndOfStream)
+                                {
+                                    string linex = process.StandardOutput.ReadLine();
+                                    Console.WriteLine(linex);
+                                }
+
+                                process.WaitForExit();
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                file.Close();
+            }
+        }
+    }
+}
---
+++ ProgressBar.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ArcToCore.Core.HandballBoard.VideoCutter
+{
+    /// <summary>
+    /// An ASCII progress bar
+    /// </summary>
+    public class ProgressBar : IDisposable, IProgress<double>
+    {
+        /// <summary>
+        /// Defines the blockCount.
+        /// </summary>
+        private const int blockCount = 10;
+
+        /// <summary>
+        /// Defines the animationInterval.
+        /// </summary>
+        private readonly TimeSpan animationInterval = TimeSpan.FromSeconds(1.0 / 8);
+
+        /// <summary>
+        /// Defines the animation.
+        /// </summary>
+        private const string animation = @"|/-\";
+
+        /// <summary>
+        /// Defines the timer.
+        /// </summary>
+        private readonly Timer timer;
+
+        /// <summary>
+        /// Defines the currentProgress.
+        /// </summary>
+        private double currentProgress = 0;
+
+        /// <summary>
+        /// Defines the currentText.
+        /// </summary>
+        private string currentText = string.Empty;
+
+        /// <summary>
+        /// Defines the disposed.
+        /// </summary>
+        private bool disposed = false;
+
+        /// <summary>
+        /// Defines the animationIndex.
+        /// </summary>
+        private int animationIndex = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressBar"/> class.
+        /// </summary>
+        public ProgressBar()
+        {
+            timer = new Timer(TimerHandler);
+
+            // A progress bar is only for temporary display in a console window.
+            // If the console output is redirected to a file, draw nothing.
+            // Otherwise, we'll end up with a lot of garbage in the target file.
+            if (!Console.IsOutputRedirected)
+            {
+                ResetTimer();
+            }
+        }
+
+        /// <summary>
+        /// The Report.
+        /// </summary>
+        /// <param name="value">The value<see cref="double"/>.</param>
+        public void Report(double value)
+        {
+            // Make sure value is in [0..1] range
+            value = Math.Max(0, Math.Min(1, value));
+            Interlocked.Exchange(ref currentProgress, value);
+        }
+
+        /// <summary>
+        /// The TimerHandler.
+        /// </summary>
+        /// <param name="state">The state<see cref="object"/>.</param>
+        private void TimerHandler(object state)
+        {
+            lock (timer)
+            {
+                if (disposed) return;
+
+                int progressBlockCount = (int)(currentProgress * blockCount);
+                int percent = (int)(currentProgress * 100);
+                string text = string.Format("[{0}{1}] {2,3}% {3}",
+                    new string('#', progressBlockCount), new string('-', blockCount - progressBlockCount),
+                    percent,
+                    animation[animationIndex++ % animation.Length]);
+                UpdateText(text);
+
+                ResetTimer();
+            }
+        }
+
+        /// <summary>
+        /// The UpdateText.
+        /// </summary>
+        /// <param name="text">The text<see cref="string"/>.</param>
+        private void UpdateText(string text)
+        {
+            // Get length of common portion
+            int commonPrefixLength = 0;
+            int commonLength = Math.Min(currentText.Length, text.Length);
+            while (commonPrefixLength < commonLength && text[commonPrefixLength] == currentText[commonPrefixLength])
+            {
+                commonPrefixLength++;
+            }
+
+            // Backtrack to the first differing character
+            StringBuilder outputBuilder = new StringBuilder();
+            outputBuilder.Append('\b', currentText.Length - commonPrefixLength);
+
+            // Output new suffix
+            outputBuilder.Append(text.Substring(commonPrefixLength));
+
+            // If the new text is shorter than the old one: delete overlapping characters
+            int overlapCount = currentText.Length - text.Length;
+            if (overlapCount > 0)
+            {
+                outputBuilder.Append(' ', overlapCount);
+                outputBuilder.Append('\b', overlapCount);
+            }
+
+            Console.Write(outputBuilder);
+            currentText = text;
+        }
+
+        /// <summary>
+        /// The ResetTimer.
+        /// </summary>
+        private void ResetTimer()
+        {
+            timer.Change(animationInterval, TimeSpan.FromMilliseconds(-1));
+        }
+
+        /// <summary>
+        /// The Dispose.
+        /// </summary>
+        public void Dispose()
+        {
+            lock (timer)
+            {
+                disposed = true;
+                UpdateText(string.Empty);
+            }
+        }
+    }
+}
